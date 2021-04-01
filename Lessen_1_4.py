4c37fd91cf36feabaf100b68b9dfb73cf1f5cce0
Rozhnov_Ilya <rozhnov85@mail.ru>
2021-04-01 21:35:45 +0500

    Доделал 4 задачу

--- Lessen_1_4.py
+++ Lessen_1_4.py
@@ -1,5 +1,12 @@
 #Пользователь вводит целое положительное число.Найдите самую большую цифру в числе. Для решения используйте цикл while и арифметические операции.
-number = input("Введите число")
+number = int(input("Введите число"))
+number_max = number % 10
+number = number // 10
+while number > 0:
+    if number % 10 > number_max:
+        number_max = number % 10
+    number = number // 10
+print(number_max)
 
 
 
